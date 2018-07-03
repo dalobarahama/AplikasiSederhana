b7feab625f48f3206e973b32480c6cce52dbea02
dalobarahama <das.barahama@gmail.com>
2018-07-03 10:06:05 +0800

    first commit

--- app/src/main/java/com/example/pengalatdite/aplikasisederhana/MainActivity.java
+++ app/src/main/java/com/example/pengalatdite/aplikasisederhana/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.pengalatdite.aplikasisederhana;
 
+//kotlin
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
