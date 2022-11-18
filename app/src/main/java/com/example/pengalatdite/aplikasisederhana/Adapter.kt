4208d0e6a767be017f45fe2f01ac9ecadebd0258
Dalo Barahama <das.barahama@gmail.com>
2022-11-18 16:26:52 +0700

    update to androidX

--- app/src/main/java/com/example/pengalatdite/aplikasisederhana/Adapter.kt
+++ app/src/main/java/com/example/pengalatdite/aplikasisederhana/Adapter.kt
@@ -1,20 +1,20 @@
 package com.example.pengalatdite.aplikasisederhana
 
 import android.content.Context
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.main.card_view.view.*
-import kotlin.coroutines.experimental.coroutineContext
 
-class Adapter(val listBerita : List<DataNews>) : RecyclerView.Adapter<Adapter.NewsViewHolder>() {
+class Adapter(private val listBerita: List<DataNews>) :
+    RecyclerView.Adapter<Adapter.NewsViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : NewsViewHolder{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.card_view, parent, false)
         return NewsViewHolder(view)
     }
@@ -28,24 +28,24 @@ class Adapter(val listBerita : List<DataNews>) : RecyclerView.Adapter<Adapter.Ne
         val news: DataNews = listBerita.get(position)
 
         Glide.with(context)
-                .asBitmap()
-                .load(news.gambarBerita)
-                .into(holder.gambarBerita)
+            .asBitmap()
+            .load(news.gambarBerita)
+            .into(holder.gambarBerita)
 
         Glide.with(context)
-                .asBitmap()
-                .load(news.gambarPenulis)
-                .into(holder.gambarPenulis)
+            .asBitmap()
+            .load(news.gambarPenulis)
+            .into(holder.gambarPenulis)
 
-        holder.deskripsiBerita.setText(news.deskripsiBerita)
-        holder.namaPenulis.setText(news.namaPenulis)
+        holder.deskripsiBerita.text = news.deskripsiBerita
+        holder.namaPenulis.text = news.namaPenulis
     }
 
     class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var deskripsiBerita : TextView = itemView.deskripsi
-        var namaPenulis : TextView = itemView.authorName
-        var gambarPenulis : CircleImageView = itemView.circleImage
-        var gambarBerita : ImageView = itemView.thumbnail
+        var deskripsiBerita: TextView = itemView.deskripsi
+        var namaPenulis: TextView = itemView.authorName
+        var gambarPenulis: CircleImageView = itemView.circleImage
+        var gambarBerita: ImageView = itemView.thumbnail
     }
 
 
--- app/src/main/java/com/example/pengalatdite/aplikasisederhana/MainActivity.kt
+++ app/src/main/java/com/example/pengalatdite/aplikasisederhana/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.pengalatdite.aplikasisederhana
 
-import android.content.Context
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,17 +11,57 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView : RecyclerView = findViewById(R.id.recylerView)
-        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+        val recyclerView: RecyclerView = findViewById(R.id.recylerView)
+        recyclerView.layoutManager =
+            LinearLayoutManager(
+                this,
+                RecyclerView.VERTICAL,
+                false
+            )
 
         val dataNews = ArrayList<DataNews>()
-        dataNews.add(DataNews("Lamborghini Aventador Direcall karena Kerusakan Kemudi", "Iron Man", R.drawable.lamborghini, R.drawable.ironman))
-        dataNews.add(DataNews("Twice Bakal Konser di Indonesia 25 Agustus 2018", "Wonder Woman", R.drawable.twice_logo, R.drawable.wonderwoman))
-        dataNews.add(DataNews("Masa Depan Tiki-taka Usai Kepergian Iniesta", "Robinhood", R.drawable.iniesta, R.drawable.robinhood))
-        dataNews.add(DataNews("Roket Jepang Meledak dan Terbakar saat Diluncurkan", "Batman", R.drawable.rocket, R.drawable.batman))
-        dataNews.add(DataNews("Gunung Agung Semburkan Lava Pijar 2 Ribu Meter", "Superman", R.drawable.gunung_agung, R.drawable.superman))
+        dataNews.add(
+            DataNews(
+                "Lamborghini Aventador Direcall karena Kerusakan Kemudi",
+                "Iron Man",
+                R.drawable.lamborghini,
+                R.drawable.ironman
+            )
+        )
+        dataNews.add(
+            DataNews(
+                "Twice Bakal Konser di Indonesia 25 Agustus 2018",
+                "Wonder Woman",
+                R.drawable.twice_logo,
+                R.drawable.wonderwoman
+            )
+        )
+        dataNews.add(
+            DataNews(
+                "Masa Depan Tiki-taka Usai Kepergian Iniesta",
+                "Robinhood",
+                R.drawable.iniesta,
+                R.drawable.robinhood
+            )
+        )
+        dataNews.add(
+            DataNews(
+                "Roket Jepang Meledak dan Terbakar saat Diluncurkan",
+                "Batman",
+                R.drawable.rocket,
+                R.drawable.batman
+            )
+        )
+        dataNews.add(
+            DataNews(
+                "Gunung Agung Semburkan Lava Pijar 2 Ribu Meter",
+                "Superman",
+                R.drawable.gunung_agung,
+                R.drawable.superman
+            )
+        )
 
-        var adapter = Adapter(dataNews)
+        val adapter = Adapter(dataNews)
         recyclerView.adapter = adapter
 
     }
