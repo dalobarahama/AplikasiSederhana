8d725ca0b1bf4269d56582495166b11b07ba021f
Dalo Barahama <das.barahama@gmail.com>
2024-10-08 16:33:18 +0700

    add link for references. data references for model.

--- app/src/main/java/com/example/pengalatdite/aplikasisederhana/DataNews.kt
+++ app/src/main/java/com/example/pengalatdite/aplikasisederhana/DataNews.kt
@@ -6,3 +6,19 @@ class DataNews(
     var gambarBerita: Int,
     var gambarPenulis: Int,
 )
+
+/**
+ * https://id.pinterest.com/pin/296956169191471520/
+ * https://id.pinterest.com/pin/20266267063390212/
+ * https://id.pinterest.com/pin/593067844707759835/
+ * https://id.pinterest.com/pin/523965737910945211/
+ *
+ * deskripsiBerita
+ * namaPenulis
+ * gambarBerita
+ * gambarPenulis
+ * dateTime i.e "3 hours ago / 23-Sep-2024"
+ * likes i.e "300"
+ * comments i.e "5"
+ * categories i.e "travel, fashion, sports"
+ */
