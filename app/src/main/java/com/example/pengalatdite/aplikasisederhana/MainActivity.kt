3a31495c68c847074086a0d1d6e63cd8e67a1e4f
Dalo Barahama <das.barahama@gmail.com>
2024-10-17 15:17:00 +0700

    set MainActivity new toolbar

--- app/src/main/java/com/example/pengalatdite/aplikasisederhana/MainActivity.kt
+++ app/src/main/java/com/example/pengalatdite/aplikasisederhana/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -51,27 +53,21 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         })
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val profilePic = findViewById<ImageView>(R.id.toolbar_profile)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(true)
+
+        profilePic.setOnClickListener {
+            startActivity(Intent(this@MainActivity, ProfileActivity::class.java))
+        }
     }
 
     override fun onStart() {
         super.onStart()
         fetchNewsData()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.profile -> {
-                startActivity(Intent(this@MainActivity, ProfileActivity::class.java))
-                true
-            }
-
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 
     private fun fetchNewsData() {
