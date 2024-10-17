f344d909d31e4e501f3c8eb8ac9d26617c199440
Dalo Barahama <das.barahama@gmail.com>
2024-10-17 22:10:36 +0700

    update ProfileActivity

--- app/src/main/java/com/example/pengalatdite/aplikasisederhana/ProfileActivity.kt
+++ app/src/main/java/com/example/pengalatdite/aplikasisederhana/ProfileActivity.kt
@@ -2,10 +2,20 @@ package com.example.pengalatdite.aplikasisederhana
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        toolbar.navigationIcon?.setTint(ContextCompat.getColor(this, R.color.colorBlack))
     }
 }
